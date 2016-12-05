a3056f4065cad0e0a2849ecac9b384ef77643287
仲金德 <bunnyfaint@me.com>
2016-12-05 22:29:46 +0800

    add testCSVManager
    and csv for test

---
+++ testCSVManager/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"../../CSVManager"
+)
+
+func main() {
+	fmt.Println("CSVManager Start...")
+
+	csvManager := new(CSVManager.CSVManager)
+	csvManager.Load("csv/params.csv")
+
+  // fmt.Println(csvManager.List.CSVs)
+	v, f := csvManager.List.Get("params", "name", "BattleConfiguration", "desc")
+	fmt.Println("v:",v,f)
+
+  vs, fs := csvManager.List.Gets("params", "name", "BattleConfiguration", "desc")
+  fmt.Println("vs:",vs,fs)
+
+
+	fmt.Println("CSVManager End...")
+}
